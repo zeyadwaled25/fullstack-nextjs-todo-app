ccfcc41773381bfb213815b47d5baef510e1b632
ZeyadWaledEbrahim <zeyadwaled165@gmail.com>
2025-09-07 22:24:45 +0300

    feat: implement dialog component with profile editing functionality and input fields

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,53 @@
 import { Button } from "@/components/ui/button";
 import { Plus } from "lucide-react";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 
 export default function Home() {
   return (
     <div className="font-sans min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <Button><Plus size={16} />Add Todo</Button>
+      <Dialog>
+        <form>
+          <DialogTrigger asChild>
+            <Button><Plus size={16} />Add Todo</Button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle>Edit profile</DialogTitle>
+              <DialogDescription>
+                Make changes to your profile here. Click save when you&apos;re
+                done.
+              </DialogDescription>
+            </DialogHeader>
+            <div className="grid gap-4">
+              <div className="grid gap-3">
+                <Label htmlFor="name-1">Name</Label>
+                <Input id="name-1" name="name" defaultValue="Pedro Duarte" />
+              </div>
+              <div className="grid gap-3">
+                <Label htmlFor="username-1">Username</Label>
+                <Input id="username-1" name="username" defaultValue="@peduarte" />
+              </div>
+            </div>
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button variant="outline">Cancel</Button>
+              </DialogClose>
+              <Button type="submit">Save changes</Button>
+            </DialogFooter>
+          </DialogContent>
+        </form>
+      </Dialog>
     </div>
   );
 }
