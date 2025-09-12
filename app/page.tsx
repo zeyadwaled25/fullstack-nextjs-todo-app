121a756880d523a5987be0aa826e4efa653b712e
ZeyadWaledEbrahim <zeyadwaled165@gmail.com>
2025-09-12 10:27:06 +0300

    feat: integrate form handling with react-hook-form and zod for todo submission

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { Button } from "@/components/ui/button";
 import { Plus } from "lucide-react";
 import {
@@ -10,20 +12,58 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { getTodosListAction } from "@/actions/todo.actions";
+import { Textarea } from "@/components/ui/textarea"
+// import { getTodosListAction } from "@/actions/todo.actions";
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useFieldArray, useForm } from "react-hook-form"
+import { z } from "zod"
 
-export default async function Home() {
-  const todos = await getTodosListAction()
+type TodoFormValues = z.infer<typeof todoFormSchema>
+
+const todoFormSchema = z.object({
+  title: z
+    .string()
+    .min(5, {
+      message: "Title must be at least 5 characters.",
+    })
+    .max(30, {
+      message: "Title must not be longer than 30 characters.",
+    }),
+  body: z
+    .string()
+    .max(80, {
+      message: "Body must not be longer than 80 characters.",
+    })
+    .optional(),
+})
+
+const defaultValues: Partial<TodoFormValues> = {
+  title: "Title",
+  body: "Body",
+}
+
+export default function Home() {
+  // const todos = await getTodosListAction()
+  const form = useForm<TodoFormValues>({
+    resolver: zodResolver(todoFormSchema),
+    defaultValues,
+    mode: "onChange",
+  })
+
+  const onSubmit = () => {}
 
   return (
     <div className="font-sans min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <div className="flex flex-col gap-4">
-        {todos.map((todo) => (
-          <div key={todo.id}>{todo.title}</div>
-        ))}
-      </div>
       <Dialog>
         <form>
           <DialogTrigger asChild>
@@ -37,15 +77,42 @@ export default async function Home() {
                 done.
               </DialogDescription>
             </DialogHeader>
-            <div className="grid gap-4">
-              <div className="grid gap-3">
-                <Label htmlFor="name-1">Name</Label>
-                <Input id="name-1" name="name" defaultValue="Pedro Duarte" />
-              </div>
-              <div className="grid gap-3">
-                <Label htmlFor="username-1">Username</Label>
-                <Input id="username-1" name="username" defaultValue="@peduarte" />
-              </div>
+            <div className="py-4">
+              <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                  <FormField
+                    control={form.control}
+                    name="title"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Title</FormLabel>
+                        <FormControl>
+                          <Input placeholder="shadcn" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+
+                  <FormField
+                    control={form.control}
+                    name="body"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Body</FormLabel>
+                        <FormControl>
+                          <Textarea
+                            placeholder="Tell us a little bit about your todo body..."
+                            className="resize-none"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </form>
+              </Form>
             </div>
             <DialogFooter>
               <DialogClose asChild>
