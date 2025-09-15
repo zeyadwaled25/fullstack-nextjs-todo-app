45c0554ee9694aeb92ef6e14ae5fbb192fb62c8a
ZeyadWaledEbrahim <zeyadwaled165@gmail.com>
2025-09-15 20:14:48 +0300

    feat: integrate TodoTable component into Home page for improved todo display

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import { getTodosListAction } from "@/actions/todo.actions";
 import AddTodoFrom from "@/components/AddTodoForm";
+import TodoTable from "@/components/TodoTable";
 
 export default async function Home() {
   const todos = await getTodosListAction()
 
   return (
     <div className="font-sans min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <AddTodoFrom />
+      <TodoTable />
       <ul>
         {todos.map((todo) => {
           return (
@@ -13,7 +16,6 @@ export default async function Home() {
           )
         })}
       </ul>
-      <AddTodoFrom />
     </div>
   );
 }
---
+++ components/TodoTable.tsx
@@ -0,0 +1,96 @@
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+import { Pen, Trash } from "lucide-react"
+import { Button } from "./ui/button"
+
+const invoices = [
+  {
+    invoice: "INV001",
+    paymentStatus: "Paid",
+    totalAmount: "$250.00",
+    paymentMethod: "Credit Card",
+  },
+  {
+    invoice: "INV002",
+    paymentStatus: "Pending",
+    totalAmount: "$150.00",
+    paymentMethod: "PayPal",
+  },
+  {
+    invoice: "INV003",
+    paymentStatus: "Unpaid",
+    totalAmount: "$350.00",
+    paymentMethod: "Bank Transfer",
+  },
+  {
+    invoice: "INV004",
+    paymentStatus: "Paid",
+    totalAmount: "$450.00",
+    paymentMethod: "Credit Card",
+  },
+  {
+    invoice: "INV005",
+    paymentStatus: "Paid",
+    totalAmount: "$550.00",
+    paymentMethod: "PayPal",
+  },
+  {
+    invoice: "INV006",
+    paymentStatus: "Pending",
+    totalAmount: "$200.00",
+    paymentMethod: "Bank Transfer",
+  },
+  {
+    invoice: "INV007",
+    paymentStatus: "Unpaid",
+    totalAmount: "$300.00",
+    paymentMethod: "Credit Card",
+  },
+]
+
+export default function TodoTable() {
+  return (
+    <Table>
+      <TableCaption>A list of todos.</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Id</TableHead>
+          <TableHead>Title</TableHead>
+          <TableHead>Completed</TableHead>
+          <TableHead className="text-right">Actions</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {invoices.map((invoice) => (
+          <TableRow key={invoice.invoice}>
+            <TableCell>68c3026300c6527668383228</TableCell>
+            <TableCell>First Todo</TableCell>
+            <TableCell>false</TableCell>
+            <TableCell className="flex items-center justify-end space-x-2">
+              <Button size={"icon"}>
+                <Pen />
+              </Button>
+              <Button size={"icon"} variant={"destructive"}>
+                <Trash />
+              </Button>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+      <TableFooter>
+        <TableRow>
+          <TableCell colSpan={3}>Total</TableCell>
+          <TableCell className="text-right">{invoices.length}</TableCell>
+        </TableRow>
+      </TableFooter>
+    </Table>
+  )
+}
